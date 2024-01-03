ba3674e71f46efe45230ccc0e2b3058e575f2c27
kwon <dohun_dev@naver.com>
2024-01-03 23:02:00 +0900

    final

--- 2024.01/Bulls and Cows/project/JS/main.js
+++ 2024.01/Bulls and Cows/project/JS/main.js
@@ -1,7 +1,8 @@
 let number;
-let number_array;
+let number_array = [];
 let count = 1;
 let isPlaying = false;
+const LEN = 3;
 const answer = document.querySelector('.input_text');
 const hint = document.querySelector('.hint_text');
 const start = document.querySelector('.button_start');
@@ -16,61 +17,80 @@ function run() { //게임 시작
         var number10 = Math.floor((number % 100) / 10);
         var number1 = number % 10;
     }
-    number_array = {number100, number10, number1};
+    for(var i=0;i<LEN;i++){
+        number_array[i] = number.toString().charAt(i);
+    }
     answer.focus();
     isPlaying = true;
     start.disabled = true;
     start.value = "게임 진행 중";
     start.style.color = 'red';
-    console.log(number);
+    //console.log(number); 콘솔창에서 정답 확인 가능
 }
 answer.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        checkMatch();
+        if(answer.value.charAt(0)==answer.value.charAt(1)||answer.value.charAt(1)==answer.value.charAt(2)||answer.value.charAt(0)==answer.value.charAt(2)) {
+            alert("각 자리에 중복된 수가 올 수 없습니다❗");
+            answer.value = "";
+        }
+        else if(answer.value.length!=3) {
+            alert("세 자릿 수만 입력할 수 있습니다❗");
+            answer.value = "";
+        }
+        else{
+            checkMatch();
+        }
     }
 });
 
 function init() {
+    count = 1;
     start.disabled = false;
     start.value = "게임시작";
-    start.style.color = 'black';
+    start.style.color = '#fff';
     hint.value = "";
 }
 function checkMatch() { //toString 활용
     if(answer.value===number.toString()){
-        alert("정답입니다😎 \n 총"+count+"번 시도로 성공하셨어요");
+        if(count==1) alert("대박!! 한 번에 정답을 맞추셨어요🥳");
+        else alert("정답입니다😎 \n 총"+count+"번 시도로 성공하셨어요");
         answer.value = "";
         init();
     }
     else {
-        //display_result();
+        display_result();
         count += 1;
     }
 }
 
-/*function display_result() {
-    for (var i = 0; i < 3; i++) { // strike 판단
-        if (parseInt(answer.value.charAt(i)) === parseInt(number_array[i])) {
+function display_result() {
+    alert("아쉽게도 틀렸어요😅 힌트를 참고해보세요!");
+    hint.value += count+". "+answer.value;
+    var cnt = 0; //out 판단하기 위한 변수
+    for (var i = 0; i < LEN; i++) { // strike 판단  
+        if (answer.value.charAt(i)===number.toString().charAt(i)) {
             hint.value += '🟡';
         }
     }
-
-    for (var i = 0; i < 3; i++) { // ball 판단
-        for (var j = 0; j < 3; j++) {
-            if (i !== j && parseInt(answer.value.charAt(i)) === parseInt(number_array[j])) {
+    for (var i=0; i<LEN; i++) { // ball 판단
+        for (var j=0; j<LEN; j++) {
+            if (i!=j && answer.value.charAt(i)==number_array[j]) {
                 hint.value += '🟢';
             }
         }
     }
-
-    var isOut = Array.from(answer.value.toString()).every(function(digit) {
-        return number_array.indexOf(parseInt(digit)) !== -1;
-    });
-
-    if (!isOut) {
-        console.log('out');
+    for (var i=0; i<LEN; i++) { //out 판단
+        for (var j=0; j<LEN; j++) {
+            if (answer.value.charAt(i) != number_array[j]) {
+                cnt += 1;
+            }
+        }
     }
-}*/
+    if(cnt==9) hint.value += '🔴';
+    hint.value += "\n";
+    answer.value="";
+}
+
 end.addEventListener('click', () => {
     alert("아쉬워요😭 정답은 "+number+"이었습니다");
     init();
