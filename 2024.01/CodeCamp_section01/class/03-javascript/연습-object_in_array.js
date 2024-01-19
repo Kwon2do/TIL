264c234c2bc12877ce5c0f9d82b31b6d546dc4d3
kwon <dohun_dev@naver.com>
2024-01-19 16:28:23 +0900

    codecamp_section01_html_css_javascript_study

---
+++ 2024.01/CodeCamp_section01/class/03-javascript/연습-object_in_array.js
@@ -0,0 +1,14 @@
+// let classmates = [
+//     { name : "철수", age: 13, school: "다람쥐초" },
+//     { name : "영희", age: 8, school: "공룡초"},
+//     { name : "훈이", age: 11, school: "거북이초등학교"}
+//     ]
+// undefined
+// classmates
+// (3) [{…}, {…}, {…}]
+// classmates.length
+// 3
+// classmates[0].name
+// '철수'
+// classmates[2].age
+// 11
