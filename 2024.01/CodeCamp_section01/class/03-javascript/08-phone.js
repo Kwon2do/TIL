7d9c1b3b8ce767202aecea48c4275d7df3f35964
kwon <dohun_dev@naver.com>
2024-01-20 23:44:13 +0900

    cyworld jukebox table and navigation bar feat

---
+++ 2024.01/CodeCamp_section01/class/03-javascript/08-phone.js
@@ -0,0 +1,11 @@
+const changeFocus = () => {
+
+    let phone1 = document.getElementById("p1").value;
+    let phone2 = document.getElementById("p2").value;
+    if(phone1.length == 3) {
+        document.getElementById("p2").focus();
+    }
+    if(phone2.length == 4) {
+        document.getElementById("p3").focus();
+    }
+}
