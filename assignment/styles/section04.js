72b5f64f62dc2e07e42b2c5f1bbd98267d12a5ce
kwon <dohun_dev@naver.com>
2024-02-02 23:25:31 +0900

    codecamp assignment project  first commit

---
+++ assignment/styles/section04.js
@@ -0,0 +1,159 @@
+import styled from "@emotion/styled";
+export const Wrapper = styled.div`
+    width:640px;
+    height:1138PX;
+    display : flex;
+    flex-direction : column;
+`
+export const Wrapper__title = styled.div`
+    padding : 20px;
+`
+export const Glassgroup = styled.div`
+    display:flex;
+    justify-content:right;
+`
+
+export const Glass = styled.img`
+    width : 23px;
+    height : 23px;
+    margin-top : 20px;
+    margin-right : 20px;
+    margin-bottom : 30px;
+`   
+export const Wrapper__header = styled.div`
+    display:flex;
+    justify-content : space-between;
+    align-items : center;
+`
+export const Title = styled.text`
+    display:inline-block;
+    line-height:64px;
+    width: 212px;
+    height: 64px;
+    font-size:30px;
+    font-weight:700;
+`
+export const Profile = styled.div`
+    display:flex;
+    justify-content : space-between;
+    margin-right : 20px;
+    align-items : center;
+`
+export const Photo = styled.img`
+    width : 60px;
+    height : 60px;
+    margin-right : 10px;
+`
+export const Name = styled.text`
+    display:inline-block;
+    text-align:center;
+    width:80px;
+    font-weight:bold;
+    width : 67px;
+    height : 35px;
+    font-weight : 700;
+    font-size : 20px;
+    line-height : 34px;
+`
+export const Next = styled.img`
+    width:17px;
+    height:17px;
+`
+
+export const Wrapper__category = styled.div`
+    display:flex;
+    margin-top:30px;
+    margin-right:80px;  
+    margin-bottom:30px;
+    justify-content : space-between;
+
+`
+export const Category = styled.text`
+    font-family: Noto Sans KR;
+    font-size: 30px;
+    font-weight: 700;
+    line-height: 43px;
+    letter-spacing: 0em;
+    text-align: center;
+    color: #CACACA;
+`
+export const Main = styled.text`
+    font-family: Noto Sans KR;
+    font-size: 30px;
+    line-height: 43px;
+    font-weight: 700;
+    letter-spacing: 0em;
+    text-align: center; 
+    color:#FF1B6D;
+    text-decoration: underline; 
+    text-decoration-skip-ink: none;
+    text-underline-offset: 10px;
+`
+export const DivideLine = styled.div`
+    width:640px;
+    height:0px;
+    border: 1px solid #CACACA;
+`
+
+export const Wrapper__body = styled.div`
+    display : flex;
+    flex-direction : column;
+    padding : 20px;
+`
+export const Group = styled.div`
+    display : flex;
+    flex-direction : column;
+    margin-bottom : 40px;
+`
+
+export const Number_Label = styled.label`
+    font-family: Noto Sans KR;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 26px;
+    letter-spacing: 0em;
+    text-align: left;
+    color:#ADADAD;
+`
+
+export const Title_Label = styled.label`
+    font-family: 'Noto Sans KR', sans-serif;
+    font-size: 24px;
+    font-weight: 400;
+    line-height: 35px;
+    letter-spacing: 0em;
+    text-align: left;
+    color:#000000;
+`
+
+export const Arrow = styled.img`
+    width : 23px;
+    height : 23px;
+`
+export const Set = styled.div`
+    display : flex;
+    justify-content : space-between;
+`
+export const Wrapper__menu = styled.div`
+    display : flex;
+    justify-content : space-around;
+`
+export const Menu = styled.div`
+    display : flex;
+    flex-direction : column;
+    justify-content : center;
+    align-items : center;
+`
+export const Icon = styled.img`
+    margin-top : 10px;
+    margin-bottom : 10px;
+`
+export const Menu_Label = styled.label`
+    font-family: Noto Sans KR;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 23px;
+    letter-spacing: 0em;
+    text-align: center;
+    color:#ADADAD;
+`
