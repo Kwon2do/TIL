05e1961d0fc1b5494b7c9a52fea3c8e00b672036
kwon <dohun_dev@naver.com>
2024-02-08 03:58:54 +0900

    Section05 Assignment05 complete

--- assignment/pages/05/05-05/index.js
+++ assignment/pages/05/05-05/index.js
@@ -1,6 +1,5 @@
 /*
 1) 아이콘 등 모든 그림은 피그마과 동일하게 만들어 주세요.
-
 2) state, setState, useState를 사용하여 이메일과 비밀번호가 빈칸인지 검증하고, 빈칸이라면 그림과 같은 메시지를 나타나게 만들어 주세요.
 3) 이메일은 @가 포함되어있는지, 비밀번호는 8자리가 넘는지 확인해 주세요
 4) 맨 윗줄의 와이파이, 배터리 등은 만들지 않습니다.
@@ -13,28 +12,83 @@ import {
   Title,
   Input,
   InputWrapper,
-  Error,
+  EmailError,
+  PasswordError,
   Group,
+  ErrorWrapper,Delete,Login,ItemWrapper,
+  Item,KakaoLogin,KakaoWrapper,KakaoLogo
 } from "../../../styles/section05-05";
 
 export default function ClassQuizPage05_05() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [emailError, setEmailError] = useState();
+  const [passwordError, setPasswordError] = useState();
+  const join = () => {
+    if(!email) {
+      setEmailError("이메일을 입력해주세요!")
+    }
+    else if(!email.includes("@")){
+      setEmailError("이메일 주소를 다시 확인해주세요!")
+    }
+    else {
+      setEmailError("")
+    }
+    if(!password) {
+      setPasswordError("비밀번호를 입력해주세요!")
+    }
+    else if(password.length < 8) {
+      setPasswordError("8~16자의 영문, 숫자, 특수 문자만 사용 가능합니다.")
+    }
+    else {
+      setPasswordError("")
+    }
+    if(!emailError && !passwordError) {
+      console.log(email)
+      console.log(password)
+      alert("로그인 완료!")
+    }
+  }
+
   return (
     <Wrapper>
       <Wrapper__title>
         <Logo src="/img/TitleLogo.png"></Logo>
         <Title>잇츠로드</Title>
       </Wrapper__title>
-
       <InputWrapper>
         <Group>
-          <Input type="text"></Input>
-          <img src="/img/delete.png" width="20px" height="20px"></img>
+          <Input type="text" onChange={(event)=>{
+            setEmail(event.target.value)
+          }}></Input>
+          <Delete src="/img/delete.png"></Delete>
         </Group>
+        <ErrorWrapper>
+          <EmailError>{emailError}</EmailError>
+        </ErrorWrapper>
       </InputWrapper>
-      <Error>이메일 주소를 다시 확인해주세요!</Error>
       <InputWrapper>
-        <Input type="text"></Input>
+        <Group>
+          <Input type="password" onChange={(event)=>{
+            setPassword(event.target.value)
+          }}></Input>
+          <Delete src="/img/delete.png"></Delete>
+        </Group>
+        <ErrorWrapper>
+          <PasswordError>{passwordError}</PasswordError>
+        </ErrorWrapper>
       </InputWrapper>
+      <Login onClick={join}>로그인</Login>
+      <ItemWrapper>
+        <Item>이메일 찾기</Item>
+        <Item>|</Item>
+        <Item>비밀번호 찾기</Item>
+        <Item>|</Item>
+        <Item>회원가입</Item>
+      </ItemWrapper>
+      <KakaoWrapper>
+        <KakaoLogin onClick={join}><KakaoLogo src="/img/KakaoLogo.png"></KakaoLogo>카카오톡으로 로그인</KakaoLogin>
+      </KakaoWrapper>
     </Wrapper>
   );
 }
--- assignment/styles/section05-05.js
+++ assignment/styles/section05-05.js
@@ -5,8 +5,11 @@ export const Wrapper = styled.div`
   height: 1138px;
   display: flex;
   flex-direction: column;
+  align-items: center;
   background-image: url("/img/background.png");
-  padding:0px 30px 0px 30px;
+  padding: 0px 50px 0px 50px;
+  background-color: rgba(0, 0, 0, 0.55);
+  background-blend-mode: multiply;
 `;
 export const Wrapper__title = styled.div`
   display: flex;
@@ -29,26 +32,98 @@ export const Title = styled.label`
 `;
 
 export const Input = styled.input`
-  width: 80%;
+  width: 100%;
   font-size: 24px;
   color: #fff;
   border: none;
   background: none;
   outline: none;
+  border-bottom: 1px solid #7d7d7d;
+  margin-bottom: 10px;
 `;
-
+export const Delete = styled.img`
+  position: relative;
+  right: 20px;
+  width: 20px;
+  height: 20px;
+`;
 export const InputWrapper = styled.div`
-    width: 540px;
-  border-bottom: 1px solid #fff;
+  width: 100%;
   padding-bottom: 20px;
   margin-bottom: 10px;
 `;
 
 export const Group = styled.div`
-    display : flex;
-    flex-direction : row;
-    justify-content : space-between;
-`
-export const Error = styled.label`
-  color: red;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
+
+export const ErrorWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const EmailError = styled.label`
+  color: #ff1b6d;
+`;
+
+export const PasswordError = styled.label`
+  color: #ff1b6d;
+`;
+
+export const Login = styled.button`
+  width: 540px;
+  height: 76px;
+  border-radius: 38px;
+  background-color: rgba(255, 27, 109, 0.6);
+  font-family: Noto Sans KR;
+  font-size: 26px;
+  font-weight: 700;
+  text-align: center;
+  color: #ffffff;
+  border: none;
+`;
+
+export const ItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 40px;
+`;
+export const Item = styled.div`
+  font-family: Noto Sans KR;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 29px;
+  letter-spacing: 0em;
+  color: #ffffff;
+  padding: 0px 20px 0px 20px;
+`;
+
+export const KakaoWrapper = styled.div`
+    display: flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    margin-top : 50px;
+`
+
+export const KakaoLogin = styled.button`
+  background-color: rgba(0, 0, 0, 0);
+  color: #ffe100;
+  width: 540px;
+  height: 76px;
+  border-radius: 38px;
+  border: 2px solid rgba(250, 225, 0, 0.6);
+  font-family: Noto Sans KR;
+  font-size: 26px;
+  font-weight: 500;
+  line-height: 38px;
+  letter-spacing: 0em;
+`;
+
+export const KakaoLogo = styled.img`
+    width:32.45px;
+    position:relative;
+    right:15px;
+    top:6px;
+`
